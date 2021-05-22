3c2c0058bd9679135220e71528ce78a467cd1870
xinchenmi <74248712+CJXCK520@users.noreply.github.com>
2021-05-22 16:01:08 +0800

    Update hostloc_get_points.py

--- hostloc_get_points.py
+++ hostloc_get_points.py
@@ -289,7 +289,7 @@ def send_message_to_tg(text: str, retry_count: int = 2):
             send_message_to_tg(text, retry_count - 1)
 
 
-@sched.scheduled_job('cron', hour=15, minute=45)
+@sched.scheduled_job('cron', hour=16, minute=05)
 def main():
     username = os.environ["HOSTLOC_USERNAME"]
     password = os.environ["HOSTLOC_PASSWORD"]
